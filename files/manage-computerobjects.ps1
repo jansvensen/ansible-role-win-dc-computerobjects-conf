ead70dd9f0e45dada1b79c3101d6205d1a6342cf
Sven Jansen <sven.jansen@devicetrust.com>
2023-05-26 09:52:03 +0200

    .

--- files/manage-computerobjects.ps1
+++ files/manage-computerobjects.ps1
@@ -8,10 +8,10 @@ function move-computer {
 
 }
 
-$ComputerObject = "t-citrix-ctrl-01"
-$TargetOU = "OU=Citrix,OU=03_Environment,OU=Computer,OU=Objects,DC=dt,DC=democloud"
+$ComputerObject = "citrix-ctrl-01"
+$TargetOU = "OU=02_Administration,OU=Computer,OU=Objects,DC=dt,DC=democloud"
 Move-computer -ComputerObject $ComputerObject -TargetOU $TargetOU
 
-$ComputerObject = "t-citrix-rdsh-01"
-$TargetOU = "OU=RDSH,OU=Citrix,OU=03_Environment,OU=Computer,OU=Objects,DC=dt,DC=democloud"
+$ComputerObject = "citrix-rdsh-01"
+$TargetOU = "OU=Citrix,OU=03_Environment,OU=Computer,OU=Objects,DC=dt,DC=democloud"
 Move-computer -ComputerObject $ComputerObject -TargetOU $TargetOU
