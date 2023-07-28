241fc5b632ce609ee8f18aeca46df204809220a3
Sven Jansen <sven.jansen@devicetrust.com>
2023-07-28 11:52:42 +0200

    .

--- files/manage-computerobjects.ps1
+++ files/manage-computerobjects.ps1
@@ -4,18 +4,18 @@ Param(
 
 $DCComputerObjects | Out-File c:\logs\lala.txt
 
-function move-computer {
-    [Parameter(Mandatory=$true)][String]$ComputerObject
-    [Parameter(Mandatory=$true)][String]$TargetOU
-    
-    $ComputerObjectDN = (Get-ADComputer $ComputerObject).DistinguishedName
-    Move-ADObject -Identity $ComputerObjectDN -TargetPath $TargetOU
-}
+#function move-computer {
+#    [Parameter(Mandatory=$true)][String]$ComputerObject
+#    [Parameter(Mandatory=$true)][String]$TargetOU
+#    
+#    $ComputerObjectDN = (Get-ADComputer $ComputerObject).DistinguishedName
+#    Move-ADObject -Identity $ComputerObjectDN -TargetPath $TargetOU
+#}
 
-$ComputerObject = "citrix-ctrl-01"
-$TargetOU = "OU=02_Administration,OU=Computer,OU=Objects,DC=dt,DC=democloud"
-Move-computer -ComputerObject $ComputerObject -TargetOU $TargetOU
+#$ComputerObject = "citrix-ctrl-01"
+#$TargetOU = "OU=02_Administration,OU=Computer,OU=Objects,DC=dt,DC=democloud"
+#Move-computer -ComputerObject $ComputerObject -TargetOU $TargetOU
 
-$ComputerObject = "citrix-rdsh-01"
-$TargetOU = "OU=Citrix,OU=03_Environment,OU=Computer,OU=Objects,DC=dt,DC=democloud"
-Move-computer -ComputerObject $ComputerObject -TargetOU $TargetOU
+#$ComputerObject = "citrix-rdsh-01"
+#$TargetOU = "OU=Citrix,OU=03_Environment,OU=Computer,OU=Objects,DC=dt,DC=democloud"
+#Move-computer -ComputerObject $ComputerObject -TargetOU $TargetOU
