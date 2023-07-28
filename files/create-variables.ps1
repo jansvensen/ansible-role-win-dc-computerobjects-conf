846ee9ab12e88c977207babae46784621b5f1901
Sven Jansen <sven.jansen@devicetrust.com>
2023-07-28 12:07:04 +0200

    .

--- files/create-variables.ps1
+++ files/create-variables.ps1
@@ -1,7 +1,6 @@
 $outputObj = @{
-
     "DCComputerObjects" = $env:dc_computer_objects
-    
 }
     
 $outputObj | Export-Clixml "C:\Logs\create-variables.xml" -Force
+$outputObj | Out-File "C:\Logs\create-variables-plain.xml" -Force
